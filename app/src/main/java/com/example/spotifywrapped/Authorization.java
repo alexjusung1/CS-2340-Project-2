005e1f938a7fb0263a4ad0d6bed3cfb706c09ddf
alexjusung1 <alexjusung@gmail.com>
2024-03-26 19:05:59 -0400

    Preparing for OAuth

--- app/src/main/java/com/example/spotifywrapped/Authorization.java
+++ app/src/main/java/com/example/spotifywrapped/Authorization.java
@@ -1,5 +1,10 @@
 package com.example.spotifywrapped;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -9,7 +14,8 @@ import java.util.Random;
 
 public class Authorization {
     static String accessToken;
-    public static void authorize() throws NoSuchAlgorithmException, MalformedURLException {
+    @NonNull
+    public static Intent getAuthorizeIntent() throws NoSuchAlgorithmException, MalformedURLException {
         String response_type = "code";
         String client_ID = "5f164b1b815e411298a2df84bae6ddbb";
         String redirect_uri = "SPOTIFY-SDK://auth";
@@ -18,25 +24,34 @@ public class Authorization {
         String code_challenge = bytesToHex(genHash());
         URL url = new URL("https://accounts.spotify.com/authorize");
 
-        String authURL = url.toString().concat("?response_type=" + response_type)
+        String authURL = url.toString()
+                .concat("?response_type=" + response_type)
                 .concat("&client_id=" + client_ID)
                 .concat("&redirect_uri=" + redirect_uri)
                 .concat("&scope=" + scope)
                 .concat("&code_challenge_method=" + code_challenge_method)
                 .concat("&code_challenge=" + code_challenge);
 
-        //IDK the web interface but assume retURL is the URL that we're redirected to after login
-        String retURL = "";
-        String[] params = retURL.split("&");
-        for (int i = 1; i < params.length; i++) {
-            if (params[i].contains("code")) {
-                accessToken = params[i].substring(params[i].indexOf("=") + 1);
-            } else if (params[i].contains("error")) {
-                throw new RuntimeException("Auth Failed");
-            }
-        }
-        accessToken = "";
+        // Move the following to a separate method (or merge into setAccessToken idk)
+//        //IDK the web interface but assume retURL is the URL that we're redirected to after login
+//        String retURL = "";
+//        String[] params = retURL.split("&");
+//        for (int i = 1; i < params.length; i++) {
+//            if (params[i].contains("code")) {
+//                accessToken = params[i].substring(params[i].indexOf("=") + 1);
+//            } else if (params[i].contains("error")) {
+//                throw new RuntimeException("Auth Failed");
+//            }
+//        }
+//        accessToken = "";
+
+        return new Intent(Intent.ACTION_VIEW, Uri.parse(authURL));
     }
+
+    public static void setAccessToken(String accessToken) {
+        Authorization.accessToken = accessToken;
+    }
+
     private static byte[] genHash() throws NoSuchAlgorithmException {
         Random random = new Random();
         String generatedString = random.ints(48, 123)
