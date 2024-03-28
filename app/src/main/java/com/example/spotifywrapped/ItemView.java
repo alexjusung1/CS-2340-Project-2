c004c6bc28e30d73f0de597ce23bc23f33a3d818
aloneztimez <bhl6878@g.rit.edu>
2024-03-28 19:56:26 -0400

    homepage

---
+++ app/src/main/java/com/example/spotifywrapped/ItemView.java
@@ -0,0 +1,19 @@
+package com.example.spotifywrapped;
+
+public class ItemView {
+    private String pastRewrap;
+
+    public ItemView (String pastRewrap) {
+        this.pastRewrap = pastRewrap;
+    }
+
+    public String getPastRewrap() {
+        return pastRewrap;
+    }
+
+    public void setPastRewrap(String pastRewrap) {
+        this.pastRewrap = pastRewrap;
+    }
+
+
+}
