c0387dd2a8f08fe20766eb3273e47480e9eb71f7
alexjusung1 <alexjusung@gmail.com>
2024-04-02 20:42:41 -0400

    fafsdadf

---
+++ app/src/main/java/com/example/spotifywrapped/SpotifyAPI.java
@@ -0,0 +1,10 @@
+package com.example.spotifywrapped;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpotifyAPI {
+    public static List<Object> getTopArtists(TimeRange range, int count) {
+        return new ArrayList<>(count);
+    }
+}
