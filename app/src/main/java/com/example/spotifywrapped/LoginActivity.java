9cfeff59598d43ef174b65cefcc5a684956b4e50
KennyC <kennyc@lawn-10-91-88-203.lawn.gatech.edu>
2024-04-02 20:49:27 -0400

    updated navigation between pages

--- app/src/main/java/com/example/spotifywrapped/LoginActivity.java
+++ app/src/main/java/com/example/spotifywrapped/LoginActivity.java
@@ -17,7 +17,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -65,7 +64,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            startActivity(new Intent(LoginActivity.this, Homepage.class));
                         } else {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
