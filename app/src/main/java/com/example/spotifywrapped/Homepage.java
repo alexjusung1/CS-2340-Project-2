aa1310c659a2c06f62dc79aa448949c4b2676336
aloneztimez <bhl6878@g.rit.edu>
2024-04-04 15:18:55 -0400

    gradient

--- app/src/main/java/com/example/spotifywrapped/Homepage.java
+++ app/src/main/java/com/example/spotifywrapped/Homepage.java
@@ -1,6 +1,7 @@
 package com.example.spotifywrapped;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,4 +45,5 @@ public class Homepage extends AppCompatActivity {
             }
         });
     }
+
 }
