16cf9b069ecb5caa98028c76137ea61c3ef20455
aloneztimez <bhl6878@g.rit.edu>
2024-04-05 18:14:00 -0400

    song frag 1

---
+++ app/src/main/java/com/example/spotifywrapped/SongFrag10.java
@@ -0,0 +1,22 @@
+package com.example.spotifywrapped;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.spotifywrapped.R;
+
+public class SongFrag10 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.song_frag_1, container, false);
+    }
+}
