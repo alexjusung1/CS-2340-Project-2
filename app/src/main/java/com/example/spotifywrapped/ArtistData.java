377c6105b5ebc91b30a365c93c7de835362c6016
alexjusung1 <alexjusung@gmail.com>
2024-04-05 16:42:52 -0400

    ArtistData template

--- app/src/main/java/com/example/spotifywrapped/ArtistData.java
+++ app/src/main/java/com/example/spotifywrapped/ArtistData.java
@@ -1,7 +1,20 @@
 package com.example.spotifywrapped;
 
+import com.google.gson.JsonObject;
+
 public class ArtistData {
     String name;
     int followerCount;
+    String artistImageURI;
+    int popularity;
 
+    public ArtistData(JsonObject jsonObject) {
+        name = jsonObject.get("name")
+                .getAsString();
+
+        followerCount = jsonObject.get("followers")
+                .getAsJsonObject()
+                .get("total")
+                .getAsInt();
+    }
 }
--- app/src/main/java/com/example/spotifywrapped/SpotifyAPI.java
+++ app/src/main/java/com/example/spotifywrapped/SpotifyAPI.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class SpotifyAPI {
     private static String topItemURL = "https://api.spotify.com/v1/me/top";
 
-    public static List<Object> getTopArtists(TimeRange range, int count) {
+    public static List<ArtistData> getTopArtists(TimeRange range, int count) {
         return new ArrayList<>(count);
     }
 }
