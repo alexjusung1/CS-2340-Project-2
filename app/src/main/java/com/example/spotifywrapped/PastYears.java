f3f83627a14ca8102eb38d5346e7b9cc13bf0226
aloneztimez <bhl6878@g.rit.edu>
2024-04-09 19:16:17 -0400

    changed past rewrap page format

--- app/src/main/java/com/example/spotifywrapped/PastYears.java
+++ app/src/main/java/com/example/spotifywrapped/PastYears.java
@@ -1,17 +1,37 @@
 package com.example.spotifywrapped;
 
 public class PastYears {
-    private String year;
+    private int year;
+    private String user;
+    private String month;
 
-    public PastYears(String year) {
+    public PastYears(String user, String month, int year) {
+        this.user = user;
+        this.month = month;
         this.year = year;
     }
 
-    public String getYear() {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(String year) {
+    public void setYear(int year) {
         this.year = year;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
     }
 }
